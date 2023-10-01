2fd93676b5288a0c6b4fbe3e099b5970c09f2d9a
EC2 Default User <ec2-user@ip-172-31-5-105.eu-west-1.compute.internal>
2023-10-01 12:10:48 +0000

    removed 0 1 dimension in pos emb

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -138,8 +138,8 @@ class LlamaRotaryEmbedding(nn.Module):
         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
         emb = torch.cat((freqs, freqs), dim=-1)
-        self.register_buffer("cos_cached", emb.cos()[None, None, :, :].to(dtype), persistent=False)
-        self.register_buffer("sin_cached", emb.sin()[None, None, :, :].to(dtype), persistent=False)
+        self.register_buffer("cos_cached", emb.cos()[:, :].to(dtype), persistent=False)
+        self.register_buffer("sin_cached", emb.sin()[:, :].to(dtype), persistent=False)
 
     def forward(self, x, seq_len=None):
         # x: [bs, num_attention_heads, seq_len, head_size]
@@ -147,8 +147,8 @@ class LlamaRotaryEmbedding(nn.Module):
             self._set_cos_sin_cache(seq_len=seq_len, device=x.device, dtype=x.dtype)
 
         return (
-            self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
-            self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
+            self.cos_cached[:seq_len, ...].to(dtype=x.dtype),
+            self.sin_cached[:seq_len, ...].to(dtype=x.dtype),
         )
 
 
@@ -210,8 +210,17 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids):
     # sin = sin.squeeze(1).squeeze(0)  # [seq_len, dim]
     # cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
     # sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
-    cos = cos[:,:,position_ids,:].squeeze(2)
-    sin = sin[:,:,position_ids,:].squeeze(2)
+    # cos = cos[:,:,position_ids,:].squeeze(2)
+    # sin = sin[:,:,position_ids,:].squeeze(2)
+    # bsz, _, _, dim = q.shape
+    # cos = cos.repeat(bsz, 1, 1, 1).squeeze(1)
+    # sin = sin.repeat(bsz, 1, 1, 1).squeeze(1)
+    # cos = torch.gather(cos, 1, position_ids.unsqueeze(2).expand(-1, -1, dim)).unsqueeze(1)
+    # sin = torch.gather(sin, 1, position_ids.unsqueeze(2).expand(-1, -1, dim)).unsqueeze(1)
+    cos = cos[position_ids, :]
+    sin = sin[position_ids, :]
+    cos = cos.unsqueeze(1)
+    sin = sin.unsqueeze(1)
     q_embed = (q * cos) + (rotate_half(q) * sin)
     k_embed = (k * cos) + (rotate_half(k) * sin)
     return q_embed, k_embed
